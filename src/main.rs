2a2bd07f0e86bb2d9c7ff900fb394834a2ab5f97
wangdong.teshin <wangdong.teshin@bytedance.com>
2024-04-20 13:40:31 +0800

    去掉没必要的包

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@
 //
 // use pkg::*;
 
-#[tokio::main]
-async fn main(){
-    wd_log::log_info_ln!("start work...");
+// #[tokio::main]
+// async fn main(){
+//     wd_log::log_info_ln!("start work...");
     // ingress::WatchIngress::default()
     //     .add_label_selector("control-class","pingora")
     //     .start_watch().await.unwrap();
+// }
+
+fn main() {
+    println!("hello world");
 }
