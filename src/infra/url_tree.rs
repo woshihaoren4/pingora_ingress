af28fecce83fc1e99869fa8cd78fee31b9d85325
sope shen <shenshouer51@gmail.com>
2024-05-29 10:59:51 +0800

    没有新增功能，优化一些代码

--- src/infra/url_tree.rs
+++ src/infra/url_tree.rs
@@ -1,137 +1,139 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
-#[derive(Clone,Debug)]
-pub struct Node<T>{
-    path:String,
-    data:Option<Arc<T>>,
-    next:HashMap<String,Node<T>>
+#[derive(Clone, Debug)]
+pub struct Node<T> {
+    path: String,
+    data: Option<Arc<T>>,
+    next: HashMap<String, Node<T>>,
 }
 
 impl<T> Default for Node<T> {
     fn default() -> Self {
-        Self{path:String::new(),data:None,next:HashMap::new()}
+        Self {
+            path: String::new(),
+            data: None,
+            next: HashMap::new(),
+        }
     }
 }
 
-impl<T> Node<T>{
-    pub fn insert_path(&mut self,path:&str,data:Arc<T>){
-        if path == ""{
+impl<T> Node<T> {
+    pub fn insert_path(&mut self, path: &str, data: Arc<T>) {
+        if path.is_empty() {
             self.data = Some(data);
             return;
         }
-        let ps = path.split('/').map(|x|if x == ""{ "*" }else { x}).rev().collect::<Vec<&str>>();
-        self.insert(ps,data);
+        let ps = path
+            .split('/')
+            .map(|x| if x.is_empty() { "*" } else { x })
+            .rev()
+            .collect::<Vec<&str>>();
+        self.insert(ps, data);
     }
-    pub fn find_by_path(&self,path:&str)->Option<Arc<T>>{
+    pub fn find_by_path(&self, path: &str) -> Option<Arc<T>> {
         let ps = path.split('/').rev().collect::<Vec<&str>>();
         self.find(ps)
     }
-    pub fn insert(&mut self, mut ps:Vec<&str>, data:Arc<T>){
-        let path = if let Some(s) = ps.pop(){
+    pub fn insert(&mut self, mut ps: Vec<&str>, data: Arc<T>) {
+        let path = if let Some(s) = ps.pop() {
             s
-        }else{
+        } else {
             return;
         };
         //先判断路径是否匹配
         if self.path.is_empty() {
             self.path = path.to_string();
-        }else if self.path.as_str() == path {
-
-        }else{
-            panic!("Url.Tree.Node.insert unknown path:{}",path);
+        } else if self.path.as_str() == path {
+        } else {
+            panic!("Url.Tree.Node.insert unknown path:{}", path);
         }
         //看下一个
-        let next = if let Some(p) = ps.last(){
+        let next = if let Some(p) = ps.last() {
             self.next.get_mut(*p)
-        }else{//不存在下一个，则修改当前
+        } else {
+            //不存在下一个，则修改当前
             self.data = Some(data);
             return;
         };
-        if let Some(next) = next{
+        if let Some(next) = next {
             //存在下一个则继续判断
-            next.insert(ps,data);
-        }else{
+            next.insert(ps, data);
+        } else {
             //不存在则新建
             let mut next = Node::<T>::default();
             next.insert(ps, data);
-            self.next.insert(next.path.clone(),next);
+            self.next.insert(next.path.clone(), next);
         }
     }
-    pub fn find(&self,mut ps:Vec<&str>)->Option<Arc<T>>{
-        let path = if let Some(s) = ps.pop(){
-            s
-        }else{
-            return None
-        };
+    pub fn find(&self, mut ps: Vec<&str>) -> Option<Arc<T>> {
+        let path = ps.pop()?;
 
         if self.path == "*" {
-
-        }else if path != self.path {
-            return self.data.clone()
+        } else if path != self.path {
+            return self.data.clone();
         }
 
-        if let Some(p) = ps.last(){
-            if let Some(next) = self.next.get(*p){
-                let res =  next.find(ps);
+        if let Some(p) = ps.last() {
+            if let Some(next) = self.next.get(*p) {
+                let res = next.find(ps);
                 if res.is_some() {
-                    return res
+                    return res;
                 }
-            }else if let Some(next ) = self.next.get("*"){
-                let res =  next.find(ps);
+            } else if let Some(next) = self.next.get("*") {
+                let res = next.find(ps);
                 if res.is_some() {
-                    return res
+                    return res;
                 }
             }
         }
-        return self.data.clone()
+        self.data.clone()
     }
 }
 
-
 #[cfg(test)]
-mod test{
-    use std::sync::Arc;
+mod test {
     use crate::infra::url_tree::Node;
+    use std::sync::Arc;
 
     #[test]
-    fn test_node(){
+    fn test_node() {
         let mut root = Node::default();
 
-        root.insert_path("/api/v1/task/create",Arc::new("avtc"));
-        root.insert_path("/api/v1/task/delete",Arc::new("avtd"));
-        root.insert_path("/api/v2/update",Arc::new("avu"));
-        root.insert_path("/api/v2/",Arc::new("av2"));
-        root.insert_path("/api",Arc::new("api"));
-        root.insert_path("/",Arc::new("default"));
-        root.insert_path("",Arc::new("null"));
+        root.insert_path("/api/v1/task/create", Arc::new("avtc"));
+        root.insert_path("/api/v1/task/delete", Arc::new("avtd"));
+        root.insert_path("/api/v2/update", Arc::new("avu"));
+        root.insert_path("/api/v2/", Arc::new("av2"));
+        root.insert_path("/api", Arc::new("api"));
+        root.insert_path("/", Arc::new("default"));
+        root.insert_path("", Arc::new("null"));
 
         let res = root.find_by_path("/api/v1/task/create").unwrap();
-        assert_eq!(*res,"avtc");
+        assert_eq!(*res, "avtc");
 
         let res = root.find_by_path("/api/v1/task/create/1234").unwrap();
-        assert_eq!(*res,"avtc");
+        assert_eq!(*res, "avtc");
 
         let res = root.find_by_path("/api/v1/task").unwrap();
-        assert_eq!(*res,"api");
+        assert_eq!(*res, "api");
 
         let res = root.find_by_path("/api/v2/task/update").unwrap();
-        assert_eq!(*res,"av2");
+        assert_eq!(*res, "av2");
 
         let res = root.find_by_path("/api/v2").unwrap();
-        assert_eq!(*res,"api");
+        assert_eq!(*res, "api");
 
         let res = root.find_by_path("/api").unwrap();
-        assert_eq!(*res,"api");
+        assert_eq!(*res, "api");
 
         let res = root.find_by_path("/").unwrap();
-        assert_eq!(*res,"default");
+        assert_eq!(*res, "default");
 
         let res = root.find_by_path("").unwrap();
-        assert_eq!(*res,"null");
+        assert_eq!(*res, "null");
 
-        root.insert_path("/api/v2/",Arc::new("api2"));
+        root.insert_path("/api/v2/", Arc::new("api2"));
         let res = root.find_by_path("/api/v2/xxx").unwrap();
-        assert_eq!(*res,"api2");
+        assert_eq!(*res, "api2");
     }
 }
--- src/pkg/pod.rs
+++ src/pkg/pod.rs
@@ -1,70 +1,70 @@
-use std::env;
-use std::fs::read_to_string;
 use k8s_openapi::api::core::v1::Pod;
 use kube::{Api, Client};
+use std::env;
+use std::fs::read_to_string;
 use wd_tools::PFErr;
 
-pub struct PodApi{
+pub struct PodApi {}
 
-}
-
-impl PodApi{
-    pub async fn get_self_pod_info()->anyhow::Result<Pod>{
+impl PodApi {
+    pub async fn get_self_pod_info() -> anyhow::Result<Pod> {
         let pod_name = PodApi::pod_name();
         if pod_name.is_empty() {
-            return anyhow::anyhow!("pod name not found").err()
-        }else{
-            wd_log::log_debug_ln!("pod name[{}]",pod_name);
+            return anyhow::anyhow!("pod name not found").err();
+        } else {
+            wd_log::log_debug_ln!("pod name[{}]", pod_name);
         }
         let namespace = PodApi::namespace();
-        if namespace.is_empty(){
-            return anyhow::anyhow!("namespace not found").err()
-        }else{
-            wd_log::log_debug_ln!("namespace name[{}]",namespace);
+        if namespace.is_empty() {
+            return anyhow::anyhow!("namespace not found").err();
+        } else {
+            wd_log::log_debug_ln!("namespace name[{}]", namespace);
         }
 
         let client = Client::try_default().await?;
-        let api = Api::<Pod>::namespaced(client,namespace.as_str());
+        let api = Api::<Pod>::namespaced(client, namespace.as_str());
 
         let pod = api.get(pod_name.as_str()).await?;
 
         Ok(pod)
     }
 
-    pub fn pod_name()->String{
+    pub fn pod_name() -> String {
         if let Ok(n) = env::var("HOSTNAME") {
-            return n
+            return n;
         }
-        if let Ok(n) = read_to_string("/etc/hostname"){
-            return n.replace("\n","")
+        if let Ok(n) = read_to_string("/etc/hostname") {
+            return n.replace('\n', "");
         }
         "".into()
     }
-    pub fn namespace()->String{
+    pub fn namespace() -> String {
         if let Ok(n) = env::var("NAMESPACE") {
-            return n
+            return n;
         }
-        if let Ok(n) = read_to_string("/run/secrets/kubernetes.io/serviceaccount/namespace"){
-            return n.replace("\n","")
+        if let Ok(n) = read_to_string("/run/secrets/kubernetes.io/serviceaccount/namespace") {
+            return n.replace('\n', "");
         }
         "".into()
     }
 }
 
-
 #[cfg(test)]
-mod test{
-    use k8s_openapi::api::core::v1::{Namespace, Pod};
+mod test {
+    use k8s_openapi::api::core::v1::Pod;
     use kube::{Api, Client};
 
     #[tokio::test]
-    pub async fn test_pod_info(){
+    pub async fn test_pod_info() {
         let client = Client::try_default().await.unwrap();
 
-        let api = Api::<Pod>::namespaced(client,"qa");
+        let api = Api::<Pod>::namespaced(client, "qa");
 
-        let pod = api.get("pingora-ingress-ctl-f4bd748c8-p7khx").await.unwrap();
+        let pod = api
+            .get("pingora-ingress-ctl-f4bd748c8-p7khx")
+            .await
+            .unwrap();
 
-        println!("pod=>{:?}",pod);
+        println!("pod=>{:?}", pod);
     }
 }
